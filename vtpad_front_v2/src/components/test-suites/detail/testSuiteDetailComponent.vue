9f612fa571ec8cac7e8160455b80718d0cd3976e
blant <ckotch47@gmail.com>
2026-05-29 20:59:59 +0300

    ui: replace v-treeview with v-list for sections, add pencil rename button

--- vtpad_front_v2/src/components/test-suites/detail/testSuiteDetailComponent.vue
+++ vtpad_front_v2/src/components/test-suites/detail/testSuiteDetailComponent.vue
@@ -96,81 +96,62 @@
             </v-card-title>
             <v-divider />
             <v-card-text class="pa-0">
-              <v-treeview
-                :items="sectionTree"
-                item-title="name"
-                item-value="id"
-                item-children="children"
-                activatable
-                @update:activated="onSectionSelect"
-                class="section-tree"
-              >
-                <template #title="{ item }">
-                  <span
-                    v-if="editingSectionId !== item.id"
-                    class="tree-node-text"
-                    @dblclick="startInlineRename(item)"
-                  >{{ item.name }}</span>
-                  <v-text-field
-                    v-else
-                    v-model="editingSectionName"
-                    density="compact"
-                    hide-details
-                    variant="outlined"
-                    autofocus
-                    class="inline-rename-field"
-                    @blur="saveInlineRename"
-                    @keydown.enter="saveInlineRename"
-                    @keydown.esc="cancelInlineRename"
-                  />
-                  <v-chip size="x-small" color="primary" variant="outlined" class="ml-2">
-                    {{ item.test_case_count || 0 }}
-                  </v-chip>
-                </template>
-                <template #prepend="{ item }">
-                  <v-icon size="small" color="primary">mdi-folder-outline</v-icon>
-                </template>
-                <template #append="{ item }">
-                  <v-btn
-                    icon="mdi-pencil"
-                    size="x-small"
-                    variant="text"
-                    class="mr-1"
-                    @click.stop="openRenameDialog(item)"
-                  />
-                  <v-menu>
-                    <template #activator="{ props: menuProps }">
-                      <v-btn
-                        icon="mdi-dots-vertical"
-                        size="x-small"
-                        variant="text"
-                        v-bind="menuProps"
-                        @click.stop
-                      />
-                    </template>
-                    <v-list density="compact">
-                      <v-list-item @click="openSectionDialog(item.id)">
-                        <template #prepend>
-                          <v-icon>mdi-plus</v-icon>
-                        </template>
-                        <v-list-item-title>Add sub-section</v-list-item-title>
-                      </v-list-item>
-                      <v-list-item @click="openRenameDialog(item)">
-                        <template #prepend>
-                          <v-icon>mdi-pencil</v-icon>
-                        </template>
-                        <v-list-item-title>Rename</v-list-item-title>
-                      </v-list-item>
-                      <v-list-item class="text-error" @click="deleteSection(item.id)">
-                        <template #prepend>
-                          <v-icon color="error">mdi-delete</v-icon>
-                        </template>
-                        <v-list-item-title>Delete</v-list-item-title>
-                      </v-list-item>
-                    </v-list>
-                  </v-menu>
-                </template>
-              </v-treeview>
+              <v-list density="compact" class="section-list">
+                <v-list-item
+                  v-for="item in flatSections"
+                  :key="item.id"
+                  :active="selectedSection?.id === item.id"
+                  rounded="sm"
+                  class="section-list-item"
+                  @click="onSectionSelect(item.id)"
+                >
+                  <template #prepend>
+                    <div class="d-flex align-center">
+                      <div :style="{ width: `${item.level * 14}px` }" />
+                      <v-icon size="small" color="primary">mdi-folder-outline</v-icon>
+                    </div>
+                  </template>
+                  <v-list-item-title class="text-body-2">
+                    {{ item.name }}
+                    <v-chip size="x-small" color="primary" variant="outlined" class="ml-2">
+                      {{ item.test_case_count || 0 }}
+                    </v-chip>
+                  </v-list-item-title>
+                  <template #append>
+                    <v-menu location="end" :close-on-content-click="true">
+                      <template #activator="{ props: menuProps }">
+                        <v-btn
+                          icon="mdi-dots-vertical"
+                          size="x-small"
+                          variant="text"
+                          v-bind="menuProps"
+                          @click.stop
+                        />
+                      </template>
+                      <v-list density="compact">
+                        <v-list-item @click="openSectionDialog(item.id)">
+                          <template #prepend>
+                            <v-icon>mdi-plus</v-icon>
+                          </template>
+                          <v-list-item-title>Add sub-section</v-list-item-title>
+                        </v-list-item>
+                        <v-list-item @click="openRenameDialog(item)">
+                          <template #prepend>
+                            <v-icon>mdi-pencil</v-icon>
+                          </template>
+                          <v-list-item-title>Rename</v-list-item-title>
+                        </v-list-item>
+                        <v-list-item class="text-error" @click="deleteSection(item.id)">
+                          <template #prepend>
+                            <v-icon color="error">mdi-delete</v-icon>
+                          </template>
+                          <v-list-item-title>Delete</v-list-item-title>
+                        </v-list-item>
+                      </v-list>
+                    </v-menu>
+                  </template>
+                </v-list-item>
+              </v-list>
             </v-card-text>
           </v-card>
         </v-col>
@@ -439,8 +420,6 @@ const sectionDialog = ref({
 const caseDialogOpen = ref(false)
 const selectedCase = ref(null)
 const breadcrumbItems = ref([])
-const editingSectionId = ref(null)
-const editingSectionName = ref('')
 
 const filteredTestCases = computed(() => {
   const q = caseSearch.value.toLowerCase().trim()
@@ -483,16 +462,15 @@ function loadAllCases() {
   })
 }
 
-function flattenTree(nodes, list = []) {
+function flattenTree(nodes, list = [], level = 0) {
   for (const n of nodes || []) {
-    list.push({ id: n.id, name: n.name })
-    flattenTree(n.children, list)
+    list.push({ id: n.id, name: n.name, level, test_case_count: n.test_case_count || 0 })
+    flattenTree(n.children, list, level + 1)
   }
   return list
 }
 
-function onSectionSelect(ids) {
-  const id = ids[0]
+function onSectionSelect(id) {
   selectedSection.value = flatSections.value.find(s => s.id === id) || { id, name: 'Section' }
   loadTestCases(id)
 }
@@ -644,27 +622,6 @@ function createRun() {
   })
 }
 
-function startInlineRename(item) {
-  editingSectionId.value = item.id
-  editingSectionName.value = item.name
-}
-
-function saveInlineRename() {
-  if (!editingSectionId.value) return
-  sectionService.update(editingSectionId.value, {
-    name: editingSectionName.value
-  }).then(() => {
-    editingSectionId.value = null
-    editingSectionName.value = ''
-    loadSections()
-  })
-}
-
-function cancelInlineRename() {
-  editingSectionId.value = null
-  editingSectionName.value = ''
-}
-
 onMounted(() => {
   loadSuite()
   loadSections()
@@ -673,7 +630,7 @@ onMounted(() => {
 </script>
 
 <style scoped>
-.section-tree :deep(.v-treeview-item) {
+.section-list-item {
   cursor: pointer;
 }
 .case-item {
